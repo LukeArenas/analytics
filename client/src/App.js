d4802e1ea3c5965b28b4f06c5e4dea906b6e9d36
Luke Arenas <lukedarenas@gmail.com>
2021-06-19 08:53:58 -0600

    set up BrowerRouter and pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,21 @@
+import { Route, useHistory, Switch } from 'react-router-dom'
+import Overview from './pages/Overview'
+import ProductDetail from './pages/ProductDetail'
+
 import './App.css'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <header>Header</header>
+      <main>
+        <Switch>
+          <Route exact path="/" component={Overview} />
+          <Route path="/:product" component={ProductDetail} />
+        </Switch>
+      </main>
+    </div>
+  )
 }
 
 export default App
